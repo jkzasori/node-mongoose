c85a870a2c8c1ab13430682f57df545699f4ba93
José Támara <jochitoel1@hotmail.com>
2018-09-03 13:23:21 -0500

    Mongoose Part 3

--- index.js
+++ index.js
@@ -3,37 +3,50 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
+
 const connect = mongoose.connect(url, {useNewUrlParser: true});
 
-connect.then((db) => {
+connect.then(() => {
+
+	const db = mongoose.connection;
 
     console.log('Connected correctly to server');
 
     Dishes.create({
         name: 'Uthapizza',
-        description: 'Test'
+        description: 'test'
     })
     .then((dish) => {
         console.log(dish);
-        
+
         return Dishes.findByIdAndUpdate(dish._id, {
-                $set: {
-                    description: 'Updated Test'
-                }
-            }, {
-                new: true
-            })
-            .exec();
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
     .then((dish) => {
         console.log(dish);
-        return db.collection('dishes').drop();
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return db.collection("dishes").drop();
     })
     .then(() => {
-        return db.close();        
+        return db.close();
     })
     .catch((err) => {
-
+        console.log(err);
     });
 
 });
